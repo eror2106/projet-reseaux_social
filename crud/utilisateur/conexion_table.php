f158fe4f40af830765df93825d6b0a724ca10458
eror2106 <etinnee974@gmail.com>
2023-01-30 18:25:45 +0400

    push

--- crud/utilisateur/conexion_table.php
+++ crud/utilisateur/conexion_table.php
@@ -8,3 +8,20 @@ $requete->execute();
 $affichage = $requete->fetchAll(PDO::FETCH_ASSOC);
 
 var_dump($affichage);
+var_dump($affichage["1"]['psudo']);
+?>
+<div style="display: flex; flex-direction:row;">
+  <?php
+  for ($j = 0; $j < sizeof($affichage); $j++) {
+
+  ?>
+    <div style="border: 1px black solid; ">
+      <?php
+      for ($i = 0; $i < 1; $i++) {
+        echo 'utilisateur n' . $j . " " . $affichage[$j]['psudo'] . "<br>";
+      }
+      ?>
+    </div>
+  <?php
+  } ?>
+</div>
