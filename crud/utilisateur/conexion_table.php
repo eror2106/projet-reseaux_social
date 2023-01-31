d1301b4827ddc9f880e084ee0bad888795628eb1
eror2106 <etinnee974@gmail.com>
2023-01-31 19:22:50 +0400

    code netoyer

--- crud/utilisateur/conexion_table.php
+++ crud/utilisateur/conexion_table.php
@@ -1,7 +1,7 @@
 <?php
 include '../connexion.php';
 $ref = $_GET['nom-user'];
-// echo $ref;
+
 $requete = $db->prepare("SELECT `psudo` FROM `identifiant`");
 $requete->execute();
 
@@ -10,12 +10,7 @@ $affichage = $requete->fetchAll(PDO::FETCH_ASSOC);
 for ($j = 0; $j < sizeof($affichage); $j++) {
 
 
-  // for ($i = 0; $i < 1; $i++) {
-  echo $affichage[$j]['psudo'] . '<br>';
-  // echo  'ref c\'esst :' . $ref . " ";
-  var_dump($ref == $affichage[$j]['psudo']);
   if ($ref == $affichage[$j]['psudo']) {
-    echo "lol" . $j . "<br>";
     header('Location:../../login.php');
     break;
   }
